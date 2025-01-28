3b4f48040be3917cab00b7ebcb025058f8037a4b
Ondřej KOUT <ondra.corner@gmail.com>
2025-01-28 12:59:06 +0100

    Navbar_1.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 const Links=[
     {href:"/profile", text:'Profile'},
