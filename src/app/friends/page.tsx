7dbe4ee203fd56ac8c232b20fcf859fb57bf673b
Ondřej KOUT <ondra.corner@gmail.com>
2025-04-08 12:08:16 +0200

    Redeploy

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -4,7 +4,6 @@ import  {getServerSession}  from "next-auth";
 import { PrismaClient } from "@prisma/client";
 import { redirect } from "next/navigation";
 export default async function Friends() {
-  const prisma = new PrismaClient();
   const session = await getServerSession(options);
   if (!session) {
     redirect ("/login");
