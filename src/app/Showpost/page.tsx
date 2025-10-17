fba86e5a7359bfee6c245356d94bcc45071a7f0f
Ondřej KOUT <ondra.corner@gmail.com>
2025-10-17 11:39:02 +0200

    Test

--- src/app/Showpost/page.tsx
+++ src/app/Showpost/page.tsx
@@ -4,17 +4,24 @@ import GeneratePost from "@/components/postsComponents/GeneratePost";
 import ShowComments from "@/components/postsComponents/ShowComments";
 import BackButton from "@/components/postsComponents/BackButton";
 
-export default async function Showpost({searchParams}: { searchParams: { postId: number }}) {
-  if (!searchParams) {
+export default async function Showpost({
+  searchParams,
+}: {
+  searchParams: Promise<{ postId: number }>;
+}) {
+  const { postId } = await searchParams;
+
+  if (!postId) {
     return <div className="text-white">Post not found</div>;
   }
-  const postID = searchParams.postId
-  console.log("postID", postID)
+
+  console.log("postID", postId);
+
   return (
     <>
-      <GeneratePost searchpostId={Number(postID)}/>
-      <ShowComments postId={Number(postID)}/>
+      <GeneratePost searchpostId={postId} />
+      <ShowComments postId={postId} />
       <BackButton />
     </>
-  )
+  );
 }
