4da59bdcae5819553e8fab327fc21e2b80e6b82d
Ondřej KOUT <ondra.corner@gmail.com>
2026-03-24 07:49:14 +0100

    Dynamic

--- src/app/Map/page.tsx
+++ src/app/Map/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import ActivitiesMapPage from '@/components/mapPageComponent'
+import dynamic from 'next/dynamic'
 import Navbar from '@/components/Navbar'
+
+const ActivitiesMapPage = dynamic(() => import('@/components/mapPageComponent'), { ssr: false })
 
 export default function Page() {
   return (
@@ -9,5 +11,5 @@ export default function Page() {
       <Navbar />
       <ActivitiesMapPage />
     </>
-  );
+  )
 }
